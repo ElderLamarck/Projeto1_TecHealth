d39ea8d64cc8efba0f91094aae87b43fc0f532d1
diegoovmoura <84737681+diegoovmoura@users.noreply.github.com>
2021-06-09 21:04:06 -0300

    tabela de verdade/ upgrade na validação de login

--- app.py
+++ app.py
@@ -1,11 +1,97 @@
-from flask import Flask, send_from_directory, request
+from flask import Flask, send_from_directory, request, jsonify
 
 app = Flask(__name__, static_url_path='')
+
+@app.route('/app/')
+def send_index():
+    return send_from_directory('.', 'index.html')
 
 @app.route('/app/<path:path>')
 def send_static(path):
     email = request.args.get('email')
-    passord = request.args.get('password')
-    if not email is None and email != 'diegoovmoura@gmail.com':
-        return send_from_directory('.', 'firstpage.html')    
+    senha = request.args.get('senha')
+    emails_validos = [
+        'diegoovmoura@gmail.com'
+    ]
+    if not email is None:
+        email_invalido = email not in emails_validos
+        senha_invalida = senha != '12345'
+        if email_invalido or senha_invalida:
+            return send_from_directory('.', 'firstpage.html')    
     return send_from_directory('.', path)
+
+@app.route('/pacientes')
+def pacientes():
+    pacientes = [
+        {
+            'nome': 'Manuela da Silva',
+            'rg': '9845098487',
+            'telefone': '(81) 3333-3333',
+            'setor': 'Enfermaria',
+            'sus': '111-2222-3333-4444-5555'
+        },
+        {
+            'nome': 'Arlindo da Silva',
+            'rg': '9845098487',
+            'telefone': '(81) 3333-3333',
+            'setor': 'Ambulatório',
+            'sus': '111-2222-3333-4444-5555'
+        },
+        {
+            'nome': 'Pedro Vieira',
+            'rg': '9845098487',
+            'telefone': '(81) 3333-3333',
+            'setor': 'COVID-19',
+            'sus': '111-2222-3333-4444-5555'
+        },
+        {
+            'nome': 'Clara Santana',
+            'rg': '9845098487',
+            'telefone': '(81) 3333-3333',
+            'setor': 'COVID-19',
+            'sus': '111-2222-3333-4444-5555'
+        },
+        {
+            'nome': 'Elder Lamarck',
+            'rg': '9845098487',
+            'telefone': '(81) 3333-3333',
+            'setor': 'COVID-19',
+            'sus': '111-2222-3333-4444-5555'
+        },
+        {
+            'nome': 'Diego Moura',
+            'rg': '9845098487',
+            'telefone': '(81) 3333-3333',
+            'setor': 'COVID-19',
+            'sus': '111-2222-3333-4444-5555'
+        },
+        {
+            'nome': 'Micaela Krolls',
+            'rg': '9845098487',
+            'telefone': '(81) 3333-3333',
+            'setor': 'COVID-19',
+            'sus': '111-2222-3333-4444-5555'
+        },
+        {
+            'nome': 'Maria Júlia Wayne',
+            'rg': '9845098487',
+            'telefone': '(81) 3333-3333',
+            'setor': 'COVID-19',
+            'sus': '111-2222-3333-4444-5555'
+        },
+        {
+            'nome': 'Jorge Camara',
+            'rg': '9845098487',
+            'telefone': '(81) 3333-3333',
+            'setor': 'COVID-19',
+            'sus': '111-2222-3333-4444-5555'
+        },
+        {
+            'nome': 'Felipe Leão',
+            'rg': '9845098487',
+            'telefone': '(81) 3333-3333',
+            'setor': 'COVID-19',
+            'sus': '111-2222-3333-4444-5555'
+        },
+    ]
+    return jsonify(pacientes)
