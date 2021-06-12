e8e70f62f9a9cadd6343cd1974a7e0bac305ce94
diegoovmoura <84737681+diegoovmoura@users.noreply.github.com>
2021-06-12 20:31:09 -0300

    upgrade da tabela

--- app.py
+++ app.py
@@ -2,6 +2,97 @@ from flask import Flask, send_from_directory, request, jsonify
 
 app = Flask(__name__, static_url_path='')
 
+lista_pacientes = [
+    {
+        'id': 1,
+        'nome': 'Manuela da Silva',
+        'rg': '9845098487',
+        'telefone': '(81) 3333-3333',
+        'setor': 'Enfermaria',
+        'sus': '111-2222-3333-4444-5555'
+    },
+    {
+        'id': 2,
+        'nome': 'Arlindo da Silva',
+        'rg': '9845098487',
+        'telefone': '(81) 3333-3333',
+        'setor': 'Ambulatório',
+        'sus': '111-2222-3333-4444-5555'
+    },
+    {
+        'id': 3,
+        'nome': 'Pedro Vieira',
+        'rg': '9845098487',
+        'telefone': '(81) 3333-3333',
+        'setor': 'COVID-19',
+        'sus': '111-2222-3333-4444-5555'
+    },
+    {
+        'id': 4,
+        'nome': 'Clara Santana',
+        'rg': '9845098487',
+        'telefone': '(81) 3333-3333',
+        'setor': 'COVID-19',
+        'sus': '111-2222-3333-4444-5555'
+    },
+    {
+        'id': 5,
+        'nome': 'Elder Lamarck',
+        'rg': '9845098487',
+        'telefone': '(81) 3333-3333',
+        'setor': 'COVID-19',
+        'sus': '111-2222-3333-4444-5555'
+    },
+    {
+        'id': 6,
+        'nome': 'Diego Moura',
+        'rg': '9845098487',
+        'telefone': '(81) 3333-3333',
+        'setor': 'COVID-19',
+        'sus': '111-2222-3333-4444-5555'
+    },
+    {
+        'id': 7,
+        'nome': 'Micaela Krolls',
+        'rg': '9845098487',
+        'telefone': '(81) 3333-3333',
+        'setor': 'COVID-19',
+        'sus': '111-2222-3333-4444-5555'
+    },
+    {
+        'id': 8,
+        'nome': 'Maria Júlia Wayne',
+        'rg': '9845098487',
+        'telefone': '(81) 3333-3333',
+        'setor': 'COVID-19',
+        'sus': '111-2222-3333-4444-5555'
+    },
+    {
+        'id': 9,
+        'nome': 'Jorge Camara',
+        'rg': '9845098487',
+        'telefone': '(81) 3333-3333',
+        'setor': 'COVID-19',
+        'sus': '111-2222-3333-4444-5555'
+    },
+    {
+        'id': 10,
+        'nome': 'Felipe Leão',
+        'rg': '9845098487',
+        'telefone': '(81) 3333-3333',
+        'setor': 'COVID-19',
+        'sus': '111-2222-3333-4444-5555'
+    },
+    {
+        'id': 11,
+        'nome': 'Ricardo FP',
+        'rg': '6666666666',
+        'telefone': '(81) 6666-6666',
+        'setor': 'COVID-19',
+        'sus': '111-2222-3333-4444-5555'
+    },
+]
+
 @app.route('/app/')
 def send_index():
     return send_from_directory('.', 'index.html')
@@ -33,83 +124,11 @@ def autenticar():
 
 @app.route('/pacientes')
 def pacientes():
-    pacientes = [
-        {
-            'nome': 'Manuela da Silva',
-            'rg': '9845098487',
-            'telefone': '(81) 3333-3333',
-            'setor': 'Enfermaria',
-            'sus': '111-2222-3333-4444-5555'
-        },
-        {
-            'nome': 'Arlindo da Silva',
-            'rg': '9845098487',
-            'telefone': '(81) 3333-3333',
-            'setor': 'Ambulatório',
-            'sus': '111-2222-3333-4444-5555'
-        },
-        {
-            'nome': 'Pedro Vieira',
-            'rg': '9845098487',
-            'telefone': '(81) 3333-3333',
-            'setor': 'COVID-19',
-            'sus': '111-2222-3333-4444-5555'
-        },
-        {
-            'nome': 'Clara Santana',
-            'rg': '9845098487',
-            'telefone': '(81) 3333-3333',
-            'setor': 'COVID-19',
-            'sus': '111-2222-3333-4444-5555'
-        },
-        {
-            'nome': 'Elder Lamarck',
-            'rg': '9845098487',
-            'telefone': '(81) 3333-3333',
-            'setor': 'COVID-19',
-            'sus': '111-2222-3333-4444-5555'
-        },
-        {
-            'nome': 'Diego Moura',
-            'rg': '9845098487',
-            'telefone': '(81) 3333-3333',
-            'setor': 'COVID-19',
-            'sus': '111-2222-3333-4444-5555'
-        },
-        {
-            'nome': 'Micaela Krolls',
-            'rg': '9845098487',
-            'telefone': '(81) 3333-3333',
-            'setor': 'COVID-19',
-            'sus': '111-2222-3333-4444-5555'
-        },
-        {
-            'nome': 'Maria Júlia Wayne',
-            'rg': '9845098487',
-            'telefone': '(81) 3333-3333',
-            'setor': 'COVID-19',
-            'sus': '111-2222-3333-4444-5555'
-        },
-        {
-            'nome': 'Jorge Camara',
-            'rg': '9845098487',
-            'telefone': '(81) 3333-3333',
-            'setor': 'COVID-19',
-            'sus': '111-2222-3333-4444-5555'
-        },
-        {
-            'nome': 'Felipe Leão',
-            'rg': '9845098487',
-            'telefone': '(81) 3333-3333',
-            'setor': 'COVID-19',
-            'sus': '111-2222-3333-4444-5555'
-        },
-        {
-            'nome': 'Desgraça FP',
-            'rg': '6666666666',
-            'telefone': '(81) 6666-6666',
-            'setor': 'COVID-19',
-            'sus': '111-2222-3333-4444-5555'
-        },
-    ]
-    return jsonify(pacientes)
+    if 'id' in request.args:
+        id = request.args['id']
+        for paciente in lista_pacientes:
+            if paciente['id'] == int(id):
+                return jsonify(paciente)
+        return jsonify(None)        
+    else:
+        return jsonify(lista_pacientes)
